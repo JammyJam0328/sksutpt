fd2ad6a74d6fe3bc234c6b10519da33186497e8d
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2022-05-30 17:09:37 +0800

    dr14

--- resources/views/components/admin-page-layout.blade.php
+++ resources/views/components/admin-page-layout.blade.php
@@ -1,8 +1,8 @@
  <div class="py-6">
-     <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+     <div class="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
          {{ $title }}
      </div>
-     <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+     <div class="px-4 mx-auto sm:px-6 md:px-8">
          <div class="py-4">
              {{ $slot }}
          </div>
