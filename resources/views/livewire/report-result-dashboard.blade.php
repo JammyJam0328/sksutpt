fc81b28ebdff5d379e81ccb3695d936ec8092b65
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2022-06-02 15:00:33 +0800

    tabel2

--- resources/views/livewire/report-result-dashboard.blade.php
+++ resources/views/livewire/report-result-dashboard.blade.php
@@ -61,7 +61,7 @@
                 <div class="flex justify-center">
                     <div class="flex flex-col">
                         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8 text-['14px']">
                                 <div>
                                     <table>
                                         <thead>
